f9ad50c89bf4ef41e797ac6fdb558ae1251c752f
Riad-ENNAIM <riad.ennaim.info@gmail.com>
2020-06-15 16:11:05 +0100

    Initial Commit: Server Setup & User Model & Authentication

---
+++ web/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+
+// Connect Database
+connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+// Define Routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
